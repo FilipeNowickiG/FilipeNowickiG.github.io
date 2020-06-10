dd004b63fe27eb22d6ee823b79244551028db0a8
FilipeNowickiG <filipe.nowicki@gmail.com>
2020-06-10 00:43:32 -0300

    Update MarchingCubes.js

--- three/MarchingCubes.js
+++ three/MarchingCubes.js
@@ -11,7 +11,7 @@ import {
 	Color,
 	ImmediateRenderObject,
 	NoColors
-} from "../../../build/three.module.js";
+} from "https://filipenowickig.github.io/three/three.module.js";
 
 var MarchingCubes = function ( resolution, material, enableUvs, enableColors ) {
 
